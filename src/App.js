b918869405f18d4ea2bbb7862db0954883d5b769
Akhilesh Yadav <akhily7525@gmail.com>
2026-03-06 14:20:54 +0530

    update react-page

--- src/App.js
+++ src/App.js
@@ -70,3 +70,35 @@ export default function Home() {
     </div>
   );
 }
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> |
+      <Link to="/about">About</Link> |
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
+
